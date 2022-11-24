7073d1427ff819b85c3f0e2db175fc3899ebf4a3
Dilavar-Davood <dilavar.davood@tarento.com>
2022-11-24 14:41:58 +0530

    another test

--- remoteGit.java
+++ remoteGit.java
@@ -1,6 +1,6 @@
 class remoteGit{
     public static void main(String[] args) {
         System.out.println("testing the git clone and remote setup");
-        System.out.println("git remote test");
+        System.out.println("git remote test and remote push");
     }
 }
